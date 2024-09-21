441573f6b886f796eac25691149e06c9ced146e0
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-09-21 14:25:30 +0500

    add single product get route

--- backend/router/productRouter.js
+++ backend/router/productRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { addProduct, getAllProducts, removeProduct } from "../controllers/ProductController.js";
+import { addProduct, getAllProducts, getProduct, removeProduct } from "../controllers/ProductController.js";
 
 const productRouter = express.Router();
 
 productRouter.get('/', getAllProducts);
 productRouter.post('/add-product', addProduct);
 productRouter.delete('/:id', removeProduct);
-
+productRouter.get('/:product_id', getProduct);
 export default productRouter;
