94a8cf195a23107ccb8447f364a412a9bba36fe0
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-09-21 21:32:58 +0500

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,18 @@ import dotenv from "dotenv";
 import notFound from "./middlewares/errorHandler.js";
 import calculateRequestProcessTime from "./middlewares/reqHandler.js";
 import productRouter from "./router/productRouter.js";
+import connectDB from "./config/db.js";
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+connectDB();
+app.use(cors());
 app.use(calculateRequestProcessTime);
 app.use(express.json());
+app.use('/api/products', productRouter);
 app.use(notFound);
-app.use('/products', productRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on Port: ${PORT}`);
