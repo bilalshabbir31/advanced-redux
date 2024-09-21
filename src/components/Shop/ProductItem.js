13b79a80587d95e9d832f0db8c979f019ebc3bf6
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-09-21 12:38:56 +0500

    add additem to cart functionality

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,21 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import { addItemToCart } from "../../store/cartSlice";
+import { useDispatch } from 'react-redux';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { title, price, description, id } = props;
+  const dispatch = useDispatch();
+
+  const addItemToCartHandler = () => {
+    dispatch(
+      addItemToCart({
+        id,
+        title,
+        price
+      })
+    );
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +26,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addItemToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
